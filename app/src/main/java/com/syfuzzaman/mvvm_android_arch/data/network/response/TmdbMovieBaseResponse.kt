ae1f80cd2d09ec9a86ecfe3999d8bba8f62de980
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2023-06-10 14:24:35 +0600

    Minor Bug Fixes

--- app/src/main/java/com/syfuzzaman/mvvm_android_arch/data/network/response/TmdbMovieBaseResponse.kt
+++ app/src/main/java/com/syfuzzaman/mvvm_android_arch/data/network/response/TmdbMovieBaseResponse.kt
@@ -3,16 +3,26 @@ package com.syfuzzaman.mvvm_android_arch.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class TmdbMovieBaseResponse (
+    @SerializedName("dates")
+    val dates: Dates? = null,
+
     @SerializedName("page")
-    val page: Int,
+    val page: Int?,
 
     @SerializedName("total_pages")
-    val total_pages: Int,
+    val total_pages: Int?,
 
     @SerializedName("results")
     val results: List<TmdbMovieResultResponse>,
 
 
     @SerializedName("total_results")
-    val total_results: Int,
+    val total_results: Int?,
+)
+
+data class Dates(
+    @SerializedName("maximum")
+    val maximum: String,
+    @SerializedName("minimum")
+    val minimum: String
 )
--- app/src/main/java/com/syfuzzaman/mvvm_android_arch/ui/home/HomeActivity.kt
+++ app/src/main/java/com/syfuzzaman/mvvm_android_arch/ui/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.syfuzzaman.mvvm_android_arch.ui.home
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -8,9 +10,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.syfuzzaman.mvvm_android_arch.R
 import com.syfuzzaman.mvvm_android_arch.databinding.ActivityHomeBinding
+import com.syfuzzaman.mvvm_android_arch.extension.observe
+import com.syfuzzaman.mvvm_android_arch.model.Resource
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val homeViewModel by viewModels<HomeViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -22,6 +30,38 @@ class HomeActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setupWithNavController(navController)
 
+        observePopularMovies()
+    }
+
+    private fun observePopularMovies(){
+        observe(homeViewModel.popularMoviesResponse){
+            when(it){
+                is Resource.Success ->{
+                    Log.d("ObservedPopularMoviesList", "Data: "+it.data)
+                }
+                is Resource.Failure ->{
+                    Log.d("ObservedPopularMoviesList", "Error: "+it.error.code+" "+it.error.msg)
+                }
+            }
+        }
+        homeViewModel.popularMoviesApi()
+    }
+
+    private fun observePosts(){
+        observe(homeViewModel.postsApiResponse){
+            when(it){
+                is Resource.Success -> {
+                    it.data.forEach {
+                        Log.d("ObservedList", it.title)
+                    }
+//                    Log.d("ObservedList", "Data: "+it.data)
+                }
+                is Resource.Failure ->{
+                    Log.d("ObservedList", "Error: "+it.error.code+" "+it.error.msg)
+                }
+            }
+        }
+        homeViewModel.postApi()
     }
 
 }
